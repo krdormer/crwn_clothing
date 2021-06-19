acb1029cfd484406a0ecd730b80453554b5e2ae2
krdormer <krdormer_dev@protonmail.com>
2021-06-19 17:09:25 -0500

    Integrate session persistence
    Added redux-persistence package to maintain a user's session and data,
    given that we want our users to keep their carts as is in the event the
    y refresh the page. Integrated redux-persistence config into store as w
    ell as root reducer. Added some items to cart and refreshed and persist
    ence seems to be persisten'

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+// Persistence 
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 import App from './App';
@@ -10,9 +12,15 @@ import App from './App';
 
 
 ReactDOM.render(
+  // Redux
   <Provider store={store} >
+    {/* Router */}
     <BrowserRouter>
-      <App />
+      {/* Persistence */}
+      <PersistGate persistor={persistor}>
+        {/* Application */}
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,12 +1,29 @@
-// Root Reducer === Overall app state \\
-
 // Function allows all reducers to be combined into root
 import { combineReducers } from "redux";
+// Persistence methods
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 // Reducer slices to combine 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+// Configuration for persistence 
+const persistConfig = {
+    // Wnere in reducer should storage begin 
+    key: 'root',
+    // Import obj from lib
+    storage,
+    // Reducers wanted for storage 
+    whitelist: ['cart']
+}
+
+// Keys represent state slices, value represents reducer for that slice
+const rootReducer = combineReducers({
+    // Handled by Firebase session, so no need for local storage as well 
     user: userReducer,
+    // Stored in whitelist
     cart: cartReducer
 });
+
+// Persistence reducer obj 
+export default persistReducer(persistConfig, rootReducer);
