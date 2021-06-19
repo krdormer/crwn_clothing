072e796616b1988d4ac5154f371f9e7d39cde846
krdormer <krdormer_dev@protonmail.com>
2021-06-19 15:25:47 -0500

    Fix bug with cartItem not displaying
    Was accidentally called selectCartItemCount instead of selectCartItems.
    Corrected code and app seems to be functional as expected up to this po
    int.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -9,29 +9,36 @@ import { withRouter } from 'react-router';
 // Redux State Components 
 import { connect } from 'react-redux';
 // Redux Selectors
-import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { createStructuredSelector } from 'reselect';
+// Redux Actions
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 
-const CartDropdown = ({ cartItems, history }) => (
-    <div className="cart-dropdown">
-        <div className="cart-items">
-            {
-                cartItems.length ? (
-                    cartItems.map(cartItem => (
-                        <CartItem key={cartItem.id} item={cartItem} />
-                    ))
-                ) : (
-                    <span className="empty-message">Your cart is empty</span>
-                )
-            }
-        </div>
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+const CartDropdown = ({ cartItems, history, dispatch }) => (
+    <div className='cart-dropdown'>
+      <div className='cart-items'>
+        {cartItems.length ? (
+          cartItems.map(cartItem => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className='empty-message'>Your cart is empty</span>
+        )}
+      </div>
+      <CustomButton
+        onClick={() => {
+          history.push('/checkout');
+          dispatch(toggleCartHidden());
+        }}
+      >
+        GO TO CHECKOUT
+      </CustomButton>
     </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-    cartItems: selectCartItemsCount
+    cartItems: selectCartItems
 });
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 // SASS Styles
 import './checkout.styles.scss';
+// Redux Components
+import { connect } from 'react-redux';
+// Redux Selector Methods 
+import { createStructuredSelector } from 'reselect';
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
-const CheckoutPage = () => (
+const CheckoutPage = ({ cartItems, total }) => (
     <div className="checkout-page">
-        CHECKOUT 
+        <div className="checkout-header">
+            <div className="header-block">
+                <span>Product</span>
+            </div>
+            <div className="header-block">
+                <span>Description</span>
+            </div>
+            <div className="header-block">
+                <span>Quantity</span>
+            </div>
+            <div className="header-block">
+                <span>Price</span>
+            </div>
+            <div className="header-block">
+                <span>Remove</span>
+            </div>
+        </div>
+        {
+            cartItems.map(cartItem => cartItem.name)
+        }
+        <div className="total">
+            <span>TOTAL: ${total}</span>
+        </div>
     </div>
 );
 
-export default CheckoutPage;
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems,
+    total: selectCartTotal
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
