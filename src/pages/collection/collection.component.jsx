744268ee4ab34e00cee823216d6f011f3149ddca
krdormer <krdormer_dev@protonmail.com>
2021-06-20 13:43:46 -0500

    Create new collection page
    Creating new collection page so as to display content dynamically, and
    more specifically to user desire. For example, shop currently displays
    4 items per category, and all together. However, after this change, the
    shop will display the category clicked on by a user, and all the items
    in that category based on dynamic url matching. Memoized selectors in
    shop.selectors.

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+// Sass Styles
+import './collection.styles.scss';
+// Components
+import CollectionItem from '../../components/collection-item/collection-item.component';
+// Redux Component
+import { connect } from 'react-redux';
+// Redux Selectors 
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+    console.log(collection)
+    return (
+    <div className="collection-page">
+        <h2 className='title'>{title}</h2>
+        <div className="item">
+            {
+                items.map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))
+            }
+        </div>
+    </div>
+)}
+
+// Ownprops = props of component which is being wrapped 
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+// Route Components
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
-
-
 
 export default ShopPage;
