34ccee4408de88c13b5a05768148f9879f3259d9
krdormer <krdormer_dev@protonmail.com>
2021-06-20 13:54:14 -0500

    Normalize shop data, memoize selectors for shop

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -14,7 +14,7 @@ const CollectionPage = ({ collection }) => {
     return (
     <div className="collection-page">
         <h2 className='title'>{title}</h2>
-        <div className="item">
+        <div className="items">
             {
                 items.map(item => (
                     <CollectionItem key={item.id} item={item} />
